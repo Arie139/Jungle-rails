e0efd78db2c0b24c59b3a24f953834d0b49c8baa
Ariel <see-har@hotmail.com>
2022-02-09 20:29:48 +0000

    added error tests to product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,18 +13,22 @@ RSpec.describe Product, type: :model do
     it 'error when product has no category' do
       @product.category = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
     it 'error when a product has no price' do
       @product.price_cents = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price cents can't be blank")
     end
     it 'error when product has no quantity' do
       @product.quantity = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
     it 'gives error when no name is inserted' do
       @product.name = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
   end
 end 
