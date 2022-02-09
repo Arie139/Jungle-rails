d4e5a224a5be920af0153a4ae477ff169a850823
Ariel <see-har@hotmail.com>
2022-02-09 20:22:03 +0000

    created spec tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before do
+      @category = Category.new(name: 'Sports')
+      @product = Product.new(name: 'Basketball', category: @category, price_cents: 40, quantity: 8)
+    end
+
+    it 'create a product' do
+      expect(@product).to be_valid
+    end
+    it 'error when product has no category' do
+      @product.category = nil
+      expect(@product).not_to be_valid
+    end
+    it 'error when a product has no price' do
+      @product.price_cents = nil
+      expect(@product).not_to be_valid
+    end
+    it 'error when product has no quantity' do
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+    end
+    it 'gives error when no name is inserted' do
+      @product.name = nil
+      expect(@product).not_to be_valid
+    end
+  end
+end 
